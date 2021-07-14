ab6213b32a67ee7ff1bd1635ce489544b43bd463
wang.haoyu <228233464@qq.com>
2021-07-14 16:37:33 +0800

    feat(service): api

--- service/app.js
+++ service/app.js
@@ -9,6 +9,10 @@ const cors = require('koa2-cors');
 const index = require('./routes/index');
 const users = require('./routes/users');
 const vue2 = require('./routes/vue2');
+const vue3 = require('./routes/vue3');
+const react15 = require('./routes/react15');
+const react16 = require('./routes/vue2');
+
 
 // error handler
 onerror(app);
@@ -35,7 +39,11 @@ app.use(async (ctx, next) => {
 // routes
 app.use(index.routes(), index.allowedMethods());
 app.use(users.routes(), users.allowedMethods());
-app.use(vue2.routes(), users.allowedMethods());
+app.use(vue2.routes(), vue2.allowedMethods());
+app.use(vue3.routes(), vue3.allowedMethods());
+app.use(react15.routes(), react15.allowedMethods());
+app.use(react16.routes(), react16.allowedMethods());
+
 
 // error-handling
 app.on('error', (err, ctx) => {
