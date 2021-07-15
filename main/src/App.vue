b37c09bcb4308f89f6527994b6cb28159fe17d7d
wang.haoyu <228233464@qq.com>
2021-07-15 12:25:23 +0800

    feat: main frame

---
+++ main/src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <Header />
+  <MainNav />
+  <div class="sub-container">
+    <Loading v-if="loading" />
+    <div v-else class="sub-body">子应用内容</div>
+  </div>
+  <Footer />
+</template>
+<script>
+import Header from './components/Header.vue'
+import MainNav from './components/MainNav.vue'
+import Loading from './components/Loading.vue'
+import Footer from './components/Footer.vue'
+
+
+export default {
+  name: "App",
+  components: {
+    Header,
+    MainNav,
+    Loading,
+    Footer
+  },
+  data() {
+    return {
+      loading: true
+    }
+  }
+}
+</script>
+<style lang="scss">
+html,
+body,
+#micro_web_main_app {
+  width: 100%;
+  height: 100%;
+}
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+.sub-container {
+  min-height: 100%;
+  position: relative;
+}
+.sub-body {
+  min-height: 0;
+  width: 100%;
+}
+</style>
---
+++ main/src/components/MainNav.vue
@@ -0,0 +1,187 @@
+<template>
+  <div class="main-nav-container">
+    <div class="main-nav-content">
+      <!-- logo内容 -->
+      <div class="main-nav-logo">
+        <img src="" alt="" />
+      </div>
+
+      <!-- 导航列表详情 -->
+      <div class="main-nav-list">
+        <div
+          v-for="(item, index) in navList"
+          :class="{ 'main-nav-active': currentIndex === index }"
+          :key="index"
+          @click="setCurrentIndex(item, index)"
+        >
+          {{ item.name }}
+        </div>
+      </div>
+
+      <!-- 搜索 -->
+      <div class="main-nav-search">
+        <div class="main-nav-search-icon">
+          <img src="../assets/blue-search.png" alt="" />
+        </div>
+        <div class="main-nav-search-input">
+          <input
+            type="text"
+            id="main-nav-search"
+            v-if="searchStatus"
+            @blur="setSearchStatus(false)"
+          />
+          <div
+            class="main-nav-search-input-fake"
+            v-else
+            @click="setSearchStatus(true)"
+          >
+            快速搜索
+          </div>
+        </div>
+        <div class="main-nav-search-button">搜索</div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import { NAV_LIST } from '../assets/js/constant'
+export default {
+  name: "MainNav",
+  data() {
+    return {
+      currentIndex: 0,
+      searchStatus: false
+    }
+  },
+  computed: {
+    navList() {
+      return NAV_LIST
+    }
+  },
+  methods: {
+    setCurrentIndex(item, index) {
+      this.currentIndex = index
+    },
+    setSearchStatus(boolean) {
+      this.searchStatus = boolean
+    }
+  }
+}
+</script>
+
+
+<style lang="scss" scoped>
+* img {
+  width: 100%;
+  height: 100%;
+}
+.main-nav {
+  &-content {
+    width: 1200px;
+    height: 100%;
+    margin: 0 auto;
+    display: flex;
+    align-items: center;
+  }
+  &-container {
+    width: 100%;
+    height: 90px;
+    background: linear-gradient(180deg, #3c6afb 0%, #75cdff 100%);
+  }
+  &-logo {
+    width: 108px;
+    height: 48px;
+    opacity: 0;
+  }
+  &-list {
+    margin: 0 136px 0 132px;
+    font-size: 24px;
+    font-weight: bold;
+    color: #ffffff;
+    display: flex;
+    height: 100%;
+    user-select: none;
+
+    & > div {
+      position: relative;
+      margin-right: 30px;
+      height: 100%;
+      display: flex;
+      align-items: center;
+      cursor: pointer;
+    }
+    & > div:last-child {
+      margin-right: 0;
+    }
+  }
+  &-active:after {
+    content: "";
+    width: 100%;
+    height: 8px;
+    background: #f7b500;
+    position: absolute;
+    bottom: 0;
+    left: 0;
+  }
+  &-search {
+    width: 260px;
+    height: 40px;
+    background: linear-gradient(90deg, #f4f8ff 0%, #e0ebff 100%) #ffffff;
+    border-radius: 8px;
+    position: relative;
+
+    &-icon {
+      width: 20px;
+      height: 20px;
+      position: absolute;
+      top: 11px;
+      left: 21px;
+    }
+
+    &-input {
+      width: 170px;
+      height: 100%;
+      position: absolute;
+      left: 41px;
+
+      &-fake {
+        width: 100%;
+        height: 100%;
+        position: absolute;
+        left: 0;
+        top: 0;
+        padding-left: 6px;
+        color: #61aee9;
+        display: flex;
+        align-items: center;
+      }
+
+      input {
+        width: 100%;
+        height: 100%;
+        border: 0;
+        outline: none;
+        font-size: 16px;
+        padding-left: 6px;
+        background: transparent;
+      }
+    }
+
+    &-button {
+      width: 52px;
+      height: 40px;
+      background: linear-gradient(90deg, #f4f8ff 0%, #e0ebff 100%) #e0ebff;
+      border-radius: 0px 8px 8px 0px;
+      position: absolute;
+      top: 0;
+      right: 0;
+      font-size: 14px;
+      color: #0091ff;
+      line-height: 19px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+  }
+}
+</style>
