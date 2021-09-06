718d20324b2a1a042f7f61b0d9931f4db01dc0f7
yyccQQu <Email.qq.com>
2021-09-06 16:51:44 +0400

    1

--- main/micro/loader/index.js
+++ main/micro/loader/index.js
@@ -8,7 +8,7 @@ export const  loadHtml = async(app) => {
     // 子应用的入口
     let entry = app.entry
 
-    const html = await parseHtml(entry)
+    const [dom, scripts] = await parseHtml(entry)
 
     const ct = document.querySelector(container)
     if(!ct){
@@ -20,15 +20,65 @@ export const  loadHtml = async(app) => {
 
 export const parseHtml = async (entry) => {
     const html = fetchResource(entry)
-    console.log(html);
+    //console.log(html);
+    let allScript = []
     
     const div = document.createElement('div')
     div.innerHTML = html
 
-    cosnt [dom, scriptUrl, script] = await parseJs()
-    return html
+    const [dom, scriptUrl, script] = await getResources(div, entry);
+    const fetchecScripts = await Promise.all(scriptUrl.map(async item => fetchResource(item)))
+
+    allScript = script.concat(fetchecScripts)
+    return [dom, allScript];
 }
  
-export const parseJs = async () => {
-    return ['', '', '']
+export const getResources = async (root, app) => {
+    const scriptUrl = [],script = [], dom = root.outerHTML
+
+    // 深度解析
+    function deepParse(element) {
+		const children = element.children;
+		const parent = element.parent;
+
+		//第一步处理位于script中的内容
+		if (element.nodeName.toLowerCase() === "script") {
+			const src = element.getAttribute("src");
+			if (!src) {
+				script.push(element.outerHTML);
+			} else {
+				if (src.startsWith("http")) {
+					scriptUrl.push(src);
+				} else {
+					//const {entry} = app;
+					scriptUrl.push(`http:${entry}/${src}`);
+				}
+			}
+
+			if (parent) {
+				parent.replaceChild(
+					document.createComment("此js文件已经被微前端替换")
+				);
+			}
+		}
+		// link 也会有js的内容
+		if (element.nodeName.toLowerCase() === "link") {
+			const href = element.getAttribute("href");
+
+			if (href.endsWith(".js")) {
+				if (href.startsWith("http")) {
+					scriptUrl.push(href);
+				} else {
+					scriptUrl.push(`http:${entry}/${src}`);
+				}
+			}
+		}
+
+        for(let i=0; i<children.length;i++){
+            deepParse(children[i])
+        }
+	}
+    deepParse(root)
+    
+    return [dom, scriptUrl, script]
 }
